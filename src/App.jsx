3ede86a02952b2de957baafc9ab87c2a78064b2f
kabuto17 <bot97316@163.com>
2023-02-18 21:57:19 +0800

    初始化

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>react-app</h2>
+    </div>
+  )
+}
+
+export default App
